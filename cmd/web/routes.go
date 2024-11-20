ef7a68e36547b5ff128776fdfbc3c163097d46a9
penhele <stephenhelk@gmail.com>
2024-11-20 14:46:20 +0700

    Initial 65

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/penhele/bookings/pkg/config"
-	"github.com/penhele/bookings/pkg/handlers"
+	"github.com/penhele/bookings/internal/config"
+	"github.com/penhele/bookings/internal/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -21,8 +21,11 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
+
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Post("/search-availability-json", handlers.Repo.AvailabilityJSON)
+
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/penhele/bookings/pkg/config"
-	"github.com/penhele/bookings/pkg/models"
-	"github.com/penhele/bookings/pkg/render"
+	"github.com/penhele/bookings/internal/config"
+	"github.com/penhele/bookings/internal/models"
+	"github.com/penhele/bookings/internal/render"
 )
 
 // Repo the repository used by the handlers
@@ -78,6 +80,27 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	end := r.Form.Get("end")
 
 	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and send JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse {
+		Ok: false,
+		Message: "Available!",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
